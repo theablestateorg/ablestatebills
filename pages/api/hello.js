68ec838c72585a03cda4b50ce1dd2046e90f2733
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-04 12:56:04 +0300

    fix: &apos;

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log(req)
+  console.log("hey")
   res.status(200).json({
     body: req.body,
     query: req.query,

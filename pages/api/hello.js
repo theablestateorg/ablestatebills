1d1fd861ab70ac8d24ca9604b57566b78b5fe904
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-05 11:42:00 +0300

    working on the handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(req.body)
-  const { day } = req.body
-  res.status(200).json(req.json());
+  console.log(res)
+  // const { day } = res.body
+  res.status(200).json({
+    body: "bingo"
+  });
 }

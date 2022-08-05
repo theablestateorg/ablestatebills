a46e09462123088f993e3709004782b2db4ae828
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-05 11:28:28 +0300

    working on the handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,5 @@
 export default function handler(req, res) {
   console.log(req.body)
   const { day } = req.body
-  res.status(200).json({
-    body: req
-  });
+  res.status(200).json(req.json());
 }

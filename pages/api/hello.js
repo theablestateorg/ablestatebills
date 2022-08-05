a5242a6dc27bf903b4e49d2cc2c3b8222cb58177
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-05 10:22:31 +0300

    fix: &apos;

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,8 +2,5 @@
 
 export default function handler(req, res) {
   console.log(req.body)
-  console.log("response ", res)
-  res.status(200).json({
-    body: {"something": "hello"}
-  });
+  res.status(200).json(req.body);
 }

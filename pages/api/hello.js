12089001bdfbf24a18c54b09d5dd2743bba94bf8
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-05 16:08:09 +0300

    working on the handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   //   day: [{id: 18,created_at:'2022-08-05T08:16:51.363114+00:00',name: 'example',website_link: 'example.com',      contact_person: 'James',telephone_number: 788934501,      status: 'active',last_paid: '2021-08-05T00:00:00',      expiry_date: '2022-08-05T00:00:00'}]
   // }
 
-  req?.day.forEach((website, index) => {
+  req.body?.day.forEach((website, index) => {
     console.log('Phone number: ', `+256${website.telephone_number}\n`,'Email: ', `${website.email}`)
     console.log(`Your website ${website.name.toUpperCase()} (${website.website_link}) will expiry in a day. Please make payment to reupdate it.`)
   })

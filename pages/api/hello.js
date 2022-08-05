1f556f5433ec6da70021a61400327c9fa2865627
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-05 16:29:40 +0300

    working on the handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const  africastalking = require('africastalking')
+require('dotenv').config()
+
+const { AFRICASTALKING_USERNAME, AFRICASTALKING_APIKEY } = process.env
+
+const client = africastalking({
+    username: AFRICASTALKING_USERNAME,
+    apiKey: AFRICASTALKING_APIKEY
+});
 
 export default function handler(req, res) {
   // console.log(req.body)
@@ -9,11 +18,20 @@ export default function handler(req, res) {
   req.body?.day.forEach((website, index) => {
     console.log('Phone number: ', `+256${website.telephone_number}\n`,'Email: ', `${website.email}`)
     console.log(`Your website ${website.name.toUpperCase()} (${website.website_link}) will expiry in a day. Please make payment to reupdate it.`)
+
+    client.SMS.send({
+      to: `+256${website.telephone_number}`,
+      message: `Your website ${website.name.toUpperCase()} (${website.website_link}) will expiry in a day. Please make payment to reupdate it.`,
+    })
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(503).json(error))
   })
+
+  
 
   // const { day } = res.body
 
-  res.status(200).json(req.body);
+  
 }
 
 // handler("hey", "hello")

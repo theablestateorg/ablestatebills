d9d780edb9612638659f2914e032864acdf303ec
Charles Kasasira <charleskasasira01@gmail.com>
2022-08-09 15:32:04 +0300

    sending emails with nodemailer

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const africastalking = require("africastalking");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
@@ -32,7 +31,7 @@ export default function handler(req, res) {
           html: `
     <div>
       <h1>Hi ${website.contact_person},</h1>
-      <p>We noticed that your website (byteofhope.com) will soon expire. Please try to update it to make sure that it stays up and running</p>
+      <p>We noticed that your website <b>(${website.website_link})</b> will expire <b>today</b>. Please try to update it to make sure that it stays up and running</p>
 
       <br />
       <br />
@@ -67,6 +66,41 @@ export default function handler(req, res) {
 
   req.body?.week &&
     req.body?.week.forEach((website, index) => {
+      transporter.sendMail(
+        {
+          from: {
+            name: "Charles",
+            address: "charleskasasira@zohomail.com",
+          },
+          to: website.email,
+          subject: `${website.name.toUpperCase()} EXPIRY DATE`,
+          // text: `Hello ${website.contact_person}, hope all is fine.`,
+          html: `
+    <div>
+      <h1>Hi ${website.contact_person},</h1>
+      <p>We noticed that your website <b>(${website.website_link})</b> will expire in <b>a week</b>. Please try to update it to make sure that it stays up and running</p>
+
+      <br />
+      <br />
+
+      <footer>
+        <p>Regards,</p>
+        <p><b>ShineAfrika Team</b></p>
+        <a href="https://shineafrika.com/">
+          www.shineafrika.com
+        </a>
+      </footer>
+    </div>`,
+        },
+        (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        }
+      );
+
       client.SMS.send({
         to: `+256${website.telephone_number}`,
         message: `Your website ${website.name.toUpperCase()} (${
@@ -79,6 +113,41 @@ export default function handler(req, res) {
 
   req.body?.month &&
     req.body?.month.forEach((website, index) => {
+      transporter.sendMail(
+        {
+          from: {
+            name: "Charles",
+            address: "charleskasasira@zohomail.com",
+          },
+          to: website.email,
+          subject: `${website.name.toUpperCase()} EXPIRY DATE`,
+          // text: `Hello ${website.contact_person}, hope all is fine.`,
+          html: `
+    <div>
+      <h1>Hi ${website.contact_person},</h1>
+      <p>We noticed that your website <b>(${website.website_link})</b> will expire in <b>a month</b>. Please try to update it to make sure that it stays up and running</p>
+
+      <br />
+      <br />
+
+      <footer>
+        <p>Regards,</p>
+        <p><b>ShineAfrika Team</b></p>
+        <a href="https://shineafrika.com/">
+          www.shineafrika.com
+        </a>
+      </footer>
+    </div>`,
+        },
+        (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        }
+      );
+
       client.SMS.send({
         to: `+256${website.telephone_number}`,
         message: `Your website ${website.name.toUpperCase()} (${
@@ -92,6 +161,41 @@ export default function handler(req, res) {
 
   req.body?.two_months &&
     req.body?.two_months.forEach((website, index) => {
+      transporter.sendMail(
+        {
+          from: {
+            name: "Charles",
+            address: "charleskasasira@zohomail.com",
+          },
+          to: website.email,
+          subject: `${website.name.toUpperCase()} EXPIRY DATE`,
+          // text: `Hello ${website.contact_person}, hope all is fine.`,
+          html: `
+    <div>
+      <h1>Hi ${website.contact_person},</h1>
+      <p>We noticed that your website <b>(${website.website_link})</b> will expire in <b>2 months</b>. Please try to update it to make sure that it stays up and running</p>
+
+      <br />
+      <br />
+
+      <footer>
+        <p>Regards,</p>
+        <p><b>ShineAfrika Team</b></p>
+        <a href="https://shineafrika.com/">
+          www.shineafrika.com
+        </a>
+      </footer>
+    </div>`,
+        },
+        (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        }
+      );
+
       client.SMS.send({
         to: `+256${website.telephone_number}`,
         message: `Your website ${website.name.toUpperCase()} (${

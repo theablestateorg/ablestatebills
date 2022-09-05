5960f6bf1648a9c1f8ffe9852a67c5e2c3648597
Charles Kasasira <charleskasasira01@gmail.com>
2022-09-05 16:44:00 +0300

    feat: bulk delete

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import { useRouter } from "next/router";
 import { MdAdd, MdSearch } from "react-icons/md";
 import { FaSort } from "react-icons/fa";
 import { supabase } from "../utils/supabase";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import moment from "moment";
+import { toast, ToastContainer } from "react-toastify";
+import { useAuth } from "../utils/auth";
+import { IoWarning } from "react-icons/io5";
 
 export default function Home({ websites }) {
   const router = useRouter();
@@ -13,7 +16,11 @@ export default function Home({ websites }) {
   const [status, setStatus] = useState("");
   const [searchBy, setSearchBy] = useState("name");
   const [sortNames, setSortNames] = useState(false);
+  const [deleteArray, setDeleteArray] = useState([])
+  const [popUp, setPopUp] = useState(false)
   const [sortBy, setSortBy] = useState("");
+  const checkbox = useRef()
+  const { user } = useAuth();
 
   websites = websites
     .filter((website) =>
@@ -31,7 +38,31 @@ export default function Home({ websites }) {
     ? websites.sort((a, b) => a[sortBy] > b[sortBy])
     : websites.sort((a, b) => b[sortBy] > a[sortBy]);
 
-  console.log(sortBy);
+  const deleteArrayIds = deleteArray.map(site => site[0].toString())
+
+  const bulkDelete = async () => {
+    const { data, error } = await supabase
+      .from('websites')
+      .delete()
+      .in('id', deleteArrayIds)
+
+      if (data) {
+        toast.success(`Successfully deleted`, { position: "top-center" });
+        await supabase
+          .from("logs")
+          .insert([
+            {
+              name: `[Bulk Deleted] ${deleteArray.map(site => site[1])}`,
+              details: `deleted by ${user.first_name} ${user.last_name}`,
+              status: "success",
+            },
+          ]);
+      }
+      if (error) {
+        toast.error(`${error?.message}`, { position: "top-center" });
+      }
+    setPopUp(false)
+  }
 
   return (
     <>
@@ -39,6 +70,9 @@ export default function Home({ websites }) {
         <title>Shine Africa</title>
       </Head>
       <Navbar />
+
+      <ToastContainer />
+
       <main className="pt-[70px] mx-3 md:mx-16 relative pb-6 min-h-screen">
         <section className="flex justify-between items-center my-10">
           <h1 className="font-bold text-2xl"></h1>
@@ -56,24 +90,41 @@ export default function Home({ websites }) {
             <caption className=" bg-white py-3 outline outline-1 outline-[#e5e7eb] px-3">
               <section className="flex justify-between items-center">
                 <h3 className="font-bold text-left">Products</h3>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center relative focus-within:text-black ">
-                    <input
-                      type="text"
-                      placeholder="search"
-                      className="px-3 py-2 bg-[#f7f7f7] rounded-lg placeholder:text-[#bcbfc2] w-full outline outline-1 outline-[#f4f3f7]"
-                      onChange={(event) => setSearchText(event.target.value)}
-                    />
+                <div className="flex items-center gap-2">
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  setPopUp(true)
+                }}>
                     <select
-                      name=""
-                      id=""
-                      className="absolute right-1 px-1 py-2 ml-2 bg-white rounded-lg outline outline-1 outline-[#ededed] text-sm"
-                      onChange={(event) => setSearchBy(event.target.value)}
-                    >
-                      <option value="name">name</option>
-                      <option value="telephone_number">no.</option>
-                      <option value="contact_person">person</option>
-                    </select>
+                        name=""
+                        id=""
+                        className="px-3 py-2 bg-[#f7f7f7] rounded-lg placeholder:text-[#bcbfc2] outline outline-1 outline-[#f4f3f7]"
+                        onChange={(event) => setSearchBy(event.target.value)} required
+                      >
+                        <option value="">Bulk Actions</option>
+                        <option value="telephone_number">Delete</option>
+                      </select>
+                      <input type="submit" value="apply" className="px-3 py-2 ml-2 bg-gray-700 text-white rounded-lg text-sm cursor-pointer" />
+                  </form>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center relative focus-within:text-black ">
+                      <input
+                        type="text"
+                        placeholder="search"
+                        className="px-3 py-2 bg-[#f7f7f7] rounded-lg placeholder:text-[#bcbfc2] w-full outline outline-1 outline-[#f4f3f7]"
+                        onChange={(event) => setSearchText(event.target.value)}
+                      />
+                      <select
+                        name=""
+                        id=""
+                        className="absolute right-1 px-1 py-2 ml-2 bg-white rounded-lg outline outline-1 outline-[#ededed] text-sm"
+                        onChange={(event) => setSearchBy(event.target.value)}
+                      >
+                        <option value="name">name</option>
+                        <option value="telephone_number">no.</option>
+                        <option value="contact_person">person</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
               </section>
@@ -81,7 +132,15 @@ export default function Home({ websites }) {
             <thead>
               <tr className="border-b bg-[#f7f7f7] text-[#555b6d]">
                 <th className="py-4 text-center px-3 w-1">
-                  <input type="checkbox" name="" id="" />
+                  <input type="checkbox" name="" id="" ref={checkbox} onChange={() => {
+                    if(checkbox.current.checked === true){
+                      Object.values(document.getElementsByClassName("checkboxes")).map(checkbox => checkbox.checked = true)
+                      setDeleteArray(websites.map(site => [site.id, site.name]))
+                    } else {
+                      Object.values(document.getElementsByClassName("checkboxes")).map(checkbox => checkbox.checked = false)
+                      setDeleteArray([])
+                    }
+                  }} />
                 </th>
                 <th className="py-4 text-left pl-3 font-light">
                   <div className="flex items-center">
@@ -147,14 +206,24 @@ export default function Home({ websites }) {
                   key={index}
                   onClick={() => router.push(`/sites/${site.id}`)}
                 >
-                  <td className="py-4 text-center px-3">
-                    <input type="checkbox" name="" id="" />
+                  <td className="py-4 text-center px-3"
+                    onClick={(event) => event.stopPropagation()}
+                  >
+                    <input type="checkbox" name="" id="" className="checkboxes"
+                      onChange={(event) => {
+                        event.stopPropagation()
+                        checkbox.current.checked = false
+                        return event.target.checked ? 
+                                  setDeleteArray([ ...deleteArray, [site.id, site.name]]) : 
+                                  setDeleteArray(deleteArray.filter(element => element[0] !== site.id))
+                      }}
+                     />
                   </td>
                   <td className="py-2 text-left pl-3">
                     <h1 className="font-medium">{site.name}</h1>
                     <span className="font-extralight text-sm text-[#bcbfc2]">
-                      last paid{" "}
-                      {moment(new Date(site.last_paid)).format("DD-MM-YYYY")}
+                      expiring on{" "}
+                      {moment(new Date(site.expiry_date)).format("DD-MM-YYYY")}
                     </span>
                   </td>
                   <td className="py-2 text-left pl-3">{site.contact_person}</td>
@@ -180,6 +249,44 @@ export default function Home({ websites }) {
             </tbody>
           </table>
         </div>
+
+        {popUp && (
+          <div
+            className={`bg-black z-20 bg-opacity-40 w-screen min-h-screen fixed top-0 left-0 right-0 flex justify-center items-center`}
+          >
+            <div className="relative bg-white dark:bg-dark-bg max-h-screen overflow-auto dark:text-secondary-text p-10  rounded-md m-2 sm:mb-5 shadow-md top-50 z-20">
+              <h1 className="text-center font-bold text-lg my-5">
+                Delete {deleteArray.length} Websites
+              </h1>
+              <p>
+                Are you sure you want to delete <b>
+                    {deleteArray.map(site => site[1] + " ")}
+                  </b>?
+              </p>
+              <p className="bg-[#ffe9d9] p-2 border-l-2 text-[#bc4c2e] border-[#fa703f] flex flex-col text-sm my-1">
+                <span className="text-[#771505] font-bold flex items-center gap-1">
+                  <IoWarning /> Warning
+                </span>
+                By deleting these website, you won&apos;t be able to access it or
+                it&apos;s info
+              </p>
+              <div className="flex justify-between mt-5">
+                <button
+                  className="outline outline-1 outline-[#1D1F20] bg-[#1D1F20] text-white py-2 px-4 hover:bg-[#1D1F20] hover:text-white flex items-center gap-2"
+                  onClick={() => setPopUp(false)}
+                >
+                  No, Cancel
+                </button>
+                <button
+                  className="outline outline-1 outline-[#1D1F20] text-[#1D1F20] py-2 px-4 hover:bg-[#1D1F20] hover:text-white flex items-center gap-2"
+                  onClick={bulkDelete}
+                >
+                  Yes, Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
 
         <footer className="text-center text-gray-500 absolute bottom-1 h-6 w-full">
           <p>

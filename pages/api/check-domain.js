8080cb729772e3e5fdf3ab68fa8009b741ec05e1
Charles Kasasira <charleskasasira01@gmail.com>
2022-10-05 17:13:48 +0300

    live search

--- pages/api/check-domain.js
+++ pages/api/check-domain.js
@@ -1,12 +1,8 @@
 const whoiser = require("whoiser");
+const axios = require("axios");
 
 const handler = async (req, res) => {
-  //const domainName = 'cloudflare.com'
-
-  const { domainName } = req.body
-  console.log(domainName)
-
-  // const domainName = "desireluzindafoundation.org";
+  const { domainName } = req.body;
 
   // retrieve WHOIS info from Registrar WHOIS servers
   const domainWhois = await whoiser(domainName, { follow: 1 });
@@ -28,34 +24,25 @@ const handler = async (req, res) => {
   }
 
   if (domainAvailability === "registered") {
-
-    res.status(200).json({ 
+    res.status(200).json({
       availability: domainAvailability,
-      created_at:  firstDomainWhois["Created Date"],
+      created_at: firstDomainWhois["Created Date"],
       expire_at: firstDomainWhois["Expiry Date"],
       server_name: firstDomainWhois["Name Server"],
-      registrar: firstDomainWhois.Registrar
+      registrar: firstDomainWhois.Registrar,
     });
-    return
+    return null;
   } else if (domainAvailability === "available") {
-    res.status(200).json({ 
+    res.status(200).json({
       availability: domainAvailability,
-      message: "This domain is available for registration right now"
+      message: "This domain is available for registration right now",
     });
-    return
+    return null;
   }
 
   // console.log(domainAvailability)
   res.status(200).json({ availability: domainAvailability });
+  return null;
 };
-
-// const whois = require('whois')
-
-// const handler = (req, res) => {
-//   whois.lookup('desireluzindafoundation.org', function(err, data) {
-//     console.log(data)
-//     res.send({"availability": data})
-//   })
-// }
 
 export default handler;

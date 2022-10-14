aec286387b67f5cc6d453c17a6067412e4ce63f8
Charles Kasasira <charleskasasira01@gmail.com>
2022-10-14 16:18:31 +0300

    fixing the internet bs link

--- pages/api/buy-domain.js
+++ pages/api/buy-domain.js
@@ -4,7 +4,7 @@ const { INTERNETBS, INTERNETACCESS } = process.env;
 
 const handler = async (req, res) => {
   try{
-    const result = await axios.post(`https://testapi.internet.bs/Domain/Create?ApiKey=${INTERNETBS}&Password=${INTERNETACCESS}&Domain=${req.body.domain}&CloneContactsFromDomain=test-api-domain11.com`);
+    const result = await axios.post(`https://api.internet.bs/Domain/Create?ApiKey=${INTERNETBS}&Password=${INTERNETACCESS}&Domain=${req.body.domain}&CloneContactsFromDomain=test-api-domain11.com`);
 
     res.json({"response": result.data});
 

61a124de08e8c8e3e48666bb5bd87220523c853c
Charles Kasasira <charleskasasira01@gmail.com>
2022-11-24 12:26:46 +0300

    serverless functions

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -13,6 +13,7 @@ const client = africastalking({
 });
 
 export default function handler(req, res) {
+  console.log(req.body)
   try {
     req.body?.day &&
       req.body?.day.forEach((website, index) => {
@@ -59,6 +60,51 @@ export default function handler(req, res) {
           message: `Your website ${website.name.toUpperCase()} (${
             website.website_link
           }) will expiry in a day. Please login in to https://shineafrika.com to make payment. If you require any further information, let us know.`,
+        })
+          .then((response) => res.status(200).json(response))
+          .catch((error) => res.status(503).json(error));
+      });
+
+
+    req.body?.day4 &&
+      req.body?.day4.forEach((website, index) => {
+        zeptoClient
+          .sendMail({
+            bounce_address: "info@bounce.shineafrika.com",
+            from: {
+              address: "noreply@shineafrika.com",
+              name: "Shine Afrika",
+            },
+            to: [
+              {
+                email_address: {
+                  address: "charleskasasira01@gmail.com",
+                },
+              },
+            ],
+            subject: `${website.name} will expire this week`,
+            textbody: "Kindly update your websites payment to keep it online",
+            htmlbody: `<html><body><h2>Hi Admin,</h2>
+          We are informing you that ${website.name} recieved a notification that <b>${website.website_link}</b> is expiring in less than a week but has not renewed it yet.
+            <br />
+            <br />
+            <footer>
+              <p>Best regards,</p>
+              <p><b>ShineAfrika Team</b></p>
+              <a href="https://shineafrika.com/">
+                www.shineafrika.com
+              </a>
+            </footer></body></html>`,
+            track_clicks: true,
+            track_opens: true,
+          })
+          .then()
+          .catch();
+
+        client.SMS.send({
+          // to: `+256${website.telephone_number}`,
+          to: website.telephone_number,
+          message: `${website.website_link} is expiring in less than a week but has not renewed it yet. `,
         })
           .then((response) => res.status(200).json(response))
           .catch((error) => res.status(503).json(error));
